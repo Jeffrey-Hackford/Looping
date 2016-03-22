873945b0f04d571c68152b5f3d64e0df55ee6cc7
Jeffrey Hackford and Niem Nguyen <niem@>
2016-03-22 13:03:34 -0700

    styled walrus page

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
   $("h1").dblclick(function() {
     alert("Header");
   });
+
   $("h2").click(function() {
     alert("Sub Heading");
   });
+
   $("img").click(function() {
     alert("Image");
   });
@@ -13,19 +15,13 @@ $(document).ready(function() {
     alert("paragraph");
   });
 
-  $(".first-child").dblclick(function() {
-    alert("first list item");
-  });
-
   $("h1").hover(function() {
     $("#walwal").fadeIn();
   });
-  $("h2").hover(function() {
-    $("#walwal").fadeOut();
-  });
 
-  $(".third-child").click(function() {
-    alert("third list item");
+  $(".clickable").click(function() {
+    $(".open").show();
+    $(".closed").toggle();
   });
 
 });
